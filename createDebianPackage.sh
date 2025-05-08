20b3e4983ac035b1d413711062950d5f0a3150d7
Florian Schmid <florian.schmid@vision-components.com>
2025-05-08 08:25:39 +0100

    Parallel built for bcm2711 and bcm2712

--- createDebianPackage.sh
+++ createDebianPackage.sh
@@ -1,10 +1,15 @@
 sudo rm -rf build
 mkdir -p build
 
+modules=("bcm2712" )
+
+
 # Set version if not set
 if [ -z "$VERSION_DEB_PACKAGE" ]; then
     export VERSION_DEB_PACKAGE="0.6.0"
 fi
+
+
 # Delete v from version
 export VERSION_DEB_PACKAGE=$(echo $VERSION_DEB_PACKAGE | sed 's/v//')
 
@@ -16,28 +21,44 @@ sed -i "s/^#define VERSION \".*\"/#define VERSION \"$VERSION_DEB_PACKAGE\"/" $VC
 sed -i "s/^#define VERSION \".*\"/#define VERSION \"$VERSION_DEB_PACKAGE\"/" $VC_CORE_FILE
 sed -i "s/^#define VERSION \".*\"/#define VERSION \"$VERSION_DEB_PACKAGE\"/" $VC_MODULES_FILE
 
+rm -rf build
+mkdir -p build
+for module in "${modules[@]}"; do
 
-rsync -a --exclude='.env' debian_package/ build/debian/
+    BUILD_DIR="build/build-$module"
+    echo "DIR: $BUILD_DIR"
+    SRC_DEB_DIR="debian_package/debian_package-$module"
+    mkdir -p $BUILD_DIR/debian/ 
+    mkdir -p $BUILD_DIR/src/
 
-mkdir -p build/debian
-rsync -a --exclude='.env' src/ build/src/
+    cp -r $SRC_DEB_DIR/* $BUILD_DIR/debian/
+    mkdir -p $BUILD_DIR/src/overlays
+    make -C overlays/overlays-$module/ all
+    cp -r overlays/overlays-$module/* $BUILD_DIR/src/overlays/
+    # cp -r overlays/overlays-$module/* $BUILD_DIR/debian/tmp/usr/src/vc-mipi-driver-$module-${VERSION_DEB_PACKAGE}/overlays/
+    rsync -a --exclude='.env' src/ $BUILD_DIR/src/
 
-DEB_BUILD_OPTIONS="KERNEL_HEADERS=$KERNEL_HEADERS" 
+    DEB_BUILD_OPTIONS="KERNEL_HEADERS=$KERNEL_HEADERS" 
+
+    envsubst '$VERSION_DEB_PACKAGE' < $SRC_DEB_DIR/changelog > $BUILD_DIR/debian/changelog
+    envsubst '$VERSION_DEB_PACKAGE' < dkms.conf > $BUILD_DIR/dkms.conf
+    envsubst '$VERSION_DEB_PACKAGE' < $SRC_DEB_DIR/control > $BUILD_DIR/debian/control
+    envsubst '$VERSION_DEB_PACKAGE' < $SRC_DEB_DIR/not-installed > $BUILD_DIR/debian/not-installed
+    envsubst '$VERSION_DEB_PACKAGE' < $SRC_DEB_DIR/postinst > $BUILD_DIR/debian/postinst
+    envsubst '$VERSION_DEB_PACKAGE' < $SRC_DEB_DIR/postrm > $BUILD_DIR/debian/postrm
+    envsubst '$VERSION_DEB_PACKAGE' < $SRC_DEB_DIR/rules > $BUILD_DIR/debian/rules
+    envsubst < $SRC_DEB_DIR/vc-mipi-driver-$module.install > $BUILD_DIR/debian/vc-mipi-driver-$module.install 
+
+    chmod ug+x $BUILD_DIR/debian/postinst
+    chmod ug+x $BUILD_DIR/debian/postrm
+    chmod ug+x $BUILD_DIR/debian/rules
+
+    cd $BUILD_DIR
+    sudo -E dpkg-buildpackage -us -uc -b
+    cd ../..
+
+done
 
 
 
-envsubst '$VERSION_DEB_PACKAGE' < debian_package/changelog > build/debian/changelog
-envsubst '$VERSION_DEB_PACKAGE' < dkms.conf > build/dkms.conf
-envsubst '$VERSION_DEB_PACKAGE' < debian_package/control > build/debian/control
-envsubst '$VERSION_DEB_PACKAGE' < debian_package/not-installed > build/debian/not-installed
-envsubst '$VERSION_DEB_PACKAGE' < debian_package/postinst > build/debian/postinst
-envsubst '$VERSION_DEB_PACKAGE' < debian_package/postrm > build/debian/postrm
-envsubst '$VERSION_DEB_PACKAGE' < debian_package/rules > build/debian/rules
-envsubst < debian_package/vc-mipi-driver-bcm2712.install > build/debian/vc-mipi-driver-bcm2712.install 
 
-chmod ug+x build/debian/postinst
-chmod ug+x build/debian/postrm
-chmod ug+x build/debian/rules
-
-cd build
-sudo -E dpkg-buildpackage -us -uc -b
